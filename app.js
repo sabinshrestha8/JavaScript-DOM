524c37ed54ea6082e16103a2f08d9abb7d7df72a
sabin <sabinshrestha8888@gmail.com>
2021-08-23 23:48:39 +0545

    Changing Text & HTML  content

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-const wmf = document.querySelector('#book-list li:nth-child(1) .name');
-// console.log(wmf);
-
-var books = document.querySelector('#book-list li .name');
-// console.log(books);
-
-books = document.querySelectorAll('#book-list li .name');
-// console.log(books);
+var books = document.querySelectorAll('#book-list li .name');
 
 Array.from(books).forEach(function (book) {
-    console.log(book);
+    // console.log(book.textContent);
+    book.textContent += ' (book title)';
 });
+
+const bookList = document.querySelector('#book-list');
+// console.log(bookList.innerHTML);
+// bookList.innerHTML = '<h2>Books and more books...</h2>';
+bookList.innerHTML += '<p>This is how you add HTML</p>';
