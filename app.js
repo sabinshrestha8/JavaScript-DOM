bc0f2b1a7c08b7895b2f6d4cdad23c66ad3b2f84
sabin <sabinshrestha8888@gmail.com>
2021-08-24 11:23:54 +0545

    JavaScript DOM - Events

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-const bookList = document.querySelector('#book-list');
+var btns = document.querySelectorAll('#book-list .delete');
 
-console.log('book-list next sibling is:', bookList.nextSibling);
-console.log('book-list next element sibling is:', bookList.nextElementSibling);
+Array.from(btns).forEach(function (btn) {
+    btn.addEventListener('click', function (e) {
 
-console.log('book-list next sibling is:', bookList.previousSibling);
-console.log('book-list next element sibling is:', bookList.previousElementSibling);
+        const li = e.target.parentElement;
+        li.parentNode.removeChild(li)
+    });
+});
 
-bookList.previousElementSibling.querySelector('p').innerHTML += '<br> Too cool for everyone else!';
+const link = document.querySelector('#page-banner a');
+
+link.addEventListener('click', function (e) {
+    e.preventDefault();
+    console.log('Navigation to', e.target.textContent, 'was prevented');
+});
