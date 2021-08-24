374f73bcd096d881038e13ff429612b0a611e31a
sabin <sabinshrestha8888@gmail.com>
2021-08-24 14:00:51 +0545

    Create elements and insert them into the DOM

--- app.js
+++ app.js
@@ -7,3 +7,26 @@ list.addEventListener('click', function (e) {
         list.removeChild(li);
     }
 });
+
+// add books
+const addForm = document.forms['add-book'];
+
+addForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const value = addForm.querySelector('input[type="text"]').value;
+
+    // create elements
+    const li = document.createElement('li');
+    const bookName = document.createElement('span');
+    const deleteBtn = document.createElement('span');
+
+    // add content
+    deleteBtn.textContent = 'delete';
+    bookName.textContent = value;
+
+    // append to the DOM(document)
+    li.appendChild(bookName);
+    li.appendChild(deleteBtn);
+    list.appendChild(li);
+});
+
