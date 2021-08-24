e83e59fd384bd44f0b5c13482d3bade7236e33bd
sabin <sabinshrestha8888@gmail.com>
2021-08-24 10:28:43 +0545

    Traverse the DOM between sibling elements & nodes

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const bookList = document.querySelector('#book-list');
 
-console.log('the parent node is:', bookList.parentNode);
-console.log('the parent element is:', bookList.parentElement.parentElement);
+console.log('book-list next sibling is:', bookList.nextSibling);
+console.log('book-list next element sibling is:', bookList.nextElementSibling);
 
-// console.log(bookList.childNodes);
-console.log(bookList.children);
+console.log('book-list next sibling is:', bookList.previousSibling);
+console.log('book-list next element sibling is:', bookList.previousElementSibling);
+
+bookList.previousElementSibling.querySelector('p').innerHTML += '<br> Too cool for everyone else!';
