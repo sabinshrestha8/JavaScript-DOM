14a0b76d2633ff7d4e53c3c7f6fd7bdfe163087b
sabin <sabinshrestha8888@gmail.com>
2021-08-24 20:09:31 +0545

    creating tabbed content box

--- app.js
+++ app.js
@@ -69,3 +69,19 @@ searchBar.addEventListener('keyup', function (e) {
         }
     })
 });
+
+// tabbed content
+const tabs = document.querySelector('.tabs');
+const panels = document.querySelectorAll('.panel');
+tabs.addEventListener('click', function (e) {
+    if (e.target.tagName == 'LI') {
+        const targetPanel = document.querySelector(e.target.dataset.target);
+        (panels).forEach(function (panel) {
+            if (panel == targetPanel) {
+                panel.classList.add('active');
+            } else {
+                panel.classList.remove('active');
+            }
+        });
+    }
+});
